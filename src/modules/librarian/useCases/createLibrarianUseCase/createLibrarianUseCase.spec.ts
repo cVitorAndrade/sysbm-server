e58eb1620e5c0b1eeb038031d59d2d64ca4e3a92
cVitorAndrade <vitorandrade1804@gmail.com>
2025-01-26 18:22:15 -0300

    test: should be able to create librarian with encrypted password

--- src/modules/librarian/useCases/createLibrarianUseCase/createLibrarianUseCase.spec.ts
+++ src/modules/librarian/useCases/createLibrarianUseCase/createLibrarianUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { compare } from 'bcrypt';
 import { LibrarianRepositoryInMemory } from '../../repositories/LibrarianRepositoryInMemory';
 import { CreateLibrarianUseCase } from './createLibrarianUseCase';
 
@@ -26,5 +27,24 @@ describe('Create librarian', () => {
     });
 
     expect(librarianRepositoryInMemory.librarians).toEqual([librarian]);
+  });
+
+  it('Should be able to create librarian with encrypted password', async () => {
+    const librarianPasswordWithoutEncryptation = '12345678';
+    const librarian = await createLibrarianUseCase.execute({
+      email: 'librarian@email.com',
+      name: 'Librarian',
+      password: '12345678',
+      permission: 'admin',
+      phoneNumber: '88988776655',
+      cpf: '00000000000',
+      status: 'active',
+    });
+
+    const librarianHasEncryptedPassword = await compare(
+      librarianPasswordWithoutEncryptation,
+      librarian.password,
+    );
+    expect(librarianHasEncryptedPassword).toBeTruthy();
   });
 });
