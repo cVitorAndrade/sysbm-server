e642401502d5f367c829d621180e97e261493455
cVitorAndrade <vitorandrade1804@gmail.com>
2025-01-28 14:48:14 -0300

    feat: add address migrate

---
+++ prisma/migrations/20250128174801_create_address/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "street" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "postalCode" TEXT NOT NULL,
+    "neighborhood" TEXT NOT NULL
+);
